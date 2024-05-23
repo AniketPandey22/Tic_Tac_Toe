973e09c3fafeb1021db4b0332455a94d2a4f8689
Aniket Pandey <anipandey2002@gmail.com>
2024-05-23 16:07:33 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './main.css'
+import { Tic_Tac_Toe } from './Tic_Tac_Toe'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Tic_Tac_Toe />
+  </React.StrictMode>,
+)
